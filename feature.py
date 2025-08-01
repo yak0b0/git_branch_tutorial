5aaec5246a053547027a3d45c7efaca18ff66f33
Jakub Zoldak <s104627@pollub.edu.pl>
2025-08-01 15:33:15 +0200

    Feature commit 1

---
+++ feature.py
@@ -0,0 +1,5 @@
+# make a program that asks the user for a list, enter something that is not a number to end list, than sort that list
+
+print("Hi, welcome to our program!")
+print("Please give your numbers, if you are done enter something that is not a number!")
+list_of_numbers = []
